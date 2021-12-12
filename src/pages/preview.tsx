c5594d8cf251a089c3c3f60582cff2b96aa70022
Ciffelia <mc.prince.0203@gmail.com>
2021-12-12 20:22:39 +0900

    Add theme and fontFamily options

--- src/pages/preview.tsx
+++ src/pages/preview.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import classNames from 'classnames';
 import { Options } from '@/utils/schema';
 
 const PreviewHead: React.VFC = () => (
@@ -26,16 +27,29 @@ const PreviewHead: React.VFC = () => (
 
 const Preview: NextPage = () => {
   const router = useRouter();
-  const parsedQuery = Options.safeParse(router.query);
 
+  const parsedQuery = Options.safeParse(router.query);
   if (!parsedQuery.success) {
     return <PreviewHead />;
   }
 
+  const options = parsedQuery.data;
+
   return (
     <>
       <PreviewHead />
-      <main className="flex flex-col justify-center items-center gap-10">
+      <main
+        className={classNames(
+          'w-screen h-screen flex flex-col justify-center items-center gap-10',
+          {
+            'bg-slate-50 text-slate-900': options.theme === 'light',
+            'bg-slate-900 text-slate-50': options.theme === 'dark',
+          },
+        )}
+        style={{
+          fontFamily: options.fontFamily,
+        }}
+      >
         <h1>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -5,7 +5,17 @@ import { z } from 'zod';
 export const FileType = z.enum(['jpeg', 'png']);
 export type FileType = z.infer<typeof FileType>;
 
+export const Theme = z.enum(['light', 'dark']).default('light');
+export type Theme = z.infer<typeof Theme>;
+
+export const FontFamily = z
+  .string()
+  .default('"Roboto Mono", "Noto Sans JP", monospace');
+export type FontFamily = z.infer<typeof FontFamily>;
+
 export const Options = z.object({
   type: FileType,
+  theme: Theme,
+  fontFamily: FontFamily,
 });
 export type Options = z.infer<typeof Options>;
