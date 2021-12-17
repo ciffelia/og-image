928cb21ed72a10b97ae4f8cd8664686a2c22c2f5
Ciffelia <mc.prince.0203@gmail.com>
2021-12-17 23:59:28 +0900

    Update @types/node

--- src/pages/api/gen.ts
+++ src/pages/api/gen.ts
@@ -13,7 +13,6 @@ const handler: NextApiHandler = async (req, res) => {
     //   'Cache-Control',
     //   `public,immutable,no-transform,s-maxage=31536000,max-age=31536000`,
     // )
-    // @ts-expect-error: wrong type definition
     .end(file);
 };
 
